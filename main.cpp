774728f17629be250ee6258f1750ffa78f1d97cc
Mihaiiii <mihaichihai03@gmail.com>
2016-02-20 15:58:59 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <QApplication>
+#include <QHBoxLayout>
+#include <QSlider>
+#include <QSpinBox>
+
+int main(int argc, char *argv[]) {
+    QApplication prog(argc, argv);
+
+    QWidget *mainWindow = new QWidget;
+    mainWindow->setWindowTitle("How many chicken wings?");
+
+    QSpinBox *spinner = new QSpinBox;
+    QSlider *slider = new QSlider(Qt::Horizontal);
+    spinner->setRange(0,100);
+    slider->setRange(0,100);
+
+    QObject::connect(spinner,SIGNAL(valueChanged(int)),
+                     slider,SLOT(setValue(int)));
+
+    QObject::connect(slider,SIGNAL(valueChanged(int)),
+                     spinner,SLOT(setValue(int)));
+
+    spinner->setValue(50);
+
+
+    QHBoxLayout *layout = new QHBoxLayout();
+     layout->addWidget(spinner);
+    layout->addWidget(slider);
+
+    mainWindow->setLayout(layout);
+    mainWindow->show();
+
+    return prog.exec();
+}
